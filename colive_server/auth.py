9902bd7cb3a20687258c3c46c167047522ea46ec
myl7 <myl.ustc@gmail.com>
2020-03-30 02:12:02 +0800

    Add model Room. Add User. Configure login manager.

--- colive_server/auth.py
+++ colive_server/auth.py
@@ -1,16 +1,45 @@
+import uuid
+import json
 import functools
 
-from flask_login import LoginManager, current_user
+from flask_login import LoginManager, current_user, UserMixin
 
 from .app import app
+from .kv_db import kv_db
 
 login_manager = LoginManager()
 login_manager.init_app(app)
 
 
+class User(UserMixin):
+    def __init__(self, user_id: str, room_id: int):
+        self.id = user_id
+        self.room_id = room_id
+
+    def __str__(self):
+        return json.dumps({
+            'id': self.id,
+            'room_id': self.room_id,
+        })
+
+    @classmethod
+    def from_str(cls, obj_str: str):
+        return cls(**json.loads(obj_str))
+
+
 @login_manager.user_loader
 def load_user(user_id: str):
-    pass
+    user_str = kv_db.get(user_id)
+    return None if user_str is None else User.from_str(user_str)
+
+
+def add_user(room_id: int):
+    user = User(str(uuid.uuid4()), room_id)
+    kv_db.set(user.id, str(user))
+
+
+def del_user(user_id: str):
+    kv_db.delete(user_id)
 
 
 def socket_login_required(f):
--- colive_server/db.py
+++ colive_server/db.py
@@ -3,3 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 from .app import app
 
 db = SQLAlchemy(app)
+
+
+class Room(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    secret = db.Column(db.String(60), nullable=False)
